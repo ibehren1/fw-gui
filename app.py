0b9057db942c8a39478106b978457798e54d013a
Isaac Behrens <isaac@behrenshome.com>
2023-12-30 18:35:30 -0600

    Updates to add multiple rules, session and retained data.

--- app.py
+++ app.py
@@ -7,204 +7,90 @@
 
     Copyright 2023 Isaac Behrens
 """
-from flask import Flask
-from flask import request
-from jinja2 import Environment
-from jinja2 import FileSystemLoader
+
+from flask import Flask, redirect, render_template, request, session, url_for
 from waitress import serve
 import os
 
+from package.functions import add_rule_to_data, delete_rule_from_data, generate_config
+
 app = Flask(__name__)
+app.secret_key = "this is the secret key"
 
 
 @app.route("/")
-def root():
-    # Open and return firewall_form.html
-    with open("firewall_form.html", "r") as f:
-        return f.read()
+def index():
+    if "firewall_name" in session:
+        return redirect(url_for("display_rules"))
+    else:
+        return redirect(url_for("login"))
 
 
-@app.route("/generate_config", methods=["POST"])
-def generate_config():
-    # Set local vars from posted form data
-    ip_version = request.form["ip_version"]
-    fw_table = request.form["fw_table"]
-    rule = request.form["rule"]
-    description = request.form["description"]
-    rule_disable = True if "rule_disable" in request.form else False
-    logging = True if "logging" in request.form else False
-    action = request.form["action"]
-    dest_address = request.form["dest_address"]
-    dest_address_type = request.form["dest_address_type"]
-    dest_port = request.form["dest_port"]
-    dest_port_type = request.form["dest_port_type"]
-    source_address = request.form["source_address"]
-    source_address_type = request.form["source_address_type"]
-    source_port = request.form["source_port"]
-    source_port_type = request.form["source_port_type"]
-    protocol = request.form["protocol"] if "protocol" in request.form else ""
-    state_est = True if "state_est" in request.form else False
-    state_inv = True if "state_inv" in request.form else False
-    state_new = True if "state_new" in request.form else False
-    state_rel = True if "state_rel" in request.form else False
+@app.route("/add_rule", methods=["POST"])
+def add_rule():
+    add_rule_to_data(session, request)
 
-    # Create firewall configuration
-    config = ["<html>", '<body style="background-color:#606263;color:aliceblue">']
+    return redirect(url_for("display_rules"))
 
-    config.append("#\n# Rule {0}\n#".format(rule))
 
-    # Disable
-    if rule_disable:
-        config.append(
-            "set firewall {0} name {1} rule {2} disable".format(
-                ip_version, fw_table, rule
-            )
+@app.route("/add_rule_form")
+def add_rule_form():
+    if "firewall_name" not in session:
+        return redirect(url_for("login"))
+    else:
+        return render_template(
+            "add_rule_form.html", firewall_name=session["firewall_name"]
         )
 
-    # Description
-    config.append(
-        "set firewall {0} name {1} rule {2} description '{3}'".format(
-            ip_version, fw_table, rule, description
+
+@app.route("/delete_rule", methods=["POST"])
+def delete_rule():
+    delete_rule_from_data(session, request)
+
+    return redirect(url_for("display_rules"))
+
+
+@app.route("/delete_rule_form")
+def delete_rule_form():
+    if "firewall_name" not in session:
+        return redirect(url_for("login"))
+    else:
+        return render_template(
+            "delete_rule_form.html", firewall_name=session["firewall_name"]
         )
+
+
+@app.route("/display_rules")
+def display_rules():
+    message = generate_config(session)
+
+    return render_template(
+        "firewall_results.html", message=message, firewall_name=session["firewall_name"]
     )
 
-    # Action
-    config.append(
-        "set firewall {0} name {1} rule {2} action {3}".format(
-            ip_version, fw_table, rule, action
-        )
-    )
 
-    # Destination
-    if dest_address != "":
-        if dest_address_type == "address":
-            config.append(
-                "set firewall {0} name {1} rule {2} destination address {3}".format(
-                    ip_version, fw_table, rule, dest_address
-                )
-            )
-        elif dest_address_type == "address_group":
-            config.append(
-                "set firewall {0} name {1} rule {2} destination group address-group {3}".format(
-                    ip_version, fw_table, rule, dest_address
-                )
-            )
-        elif dest_address_type == "network_group":
-            config.append(
-                "set firewall {0} name {1} rule {2} destination group network-group {3}".format(
-                    ip_version, fw_table, rule, dest_address
-                )
-            )
-    if dest_port != "":
-        if dest_port_type == "port":
-            config.append(
-                "set firewall {0} name {1} rule {2} destination port {3}".format(
-                    ip_version, fw_table, rule, dest_port
-                )
-            )
-        elif dest_port_type == "port_group":
-            config.append(
-                "set firewall {0} name {1} rule {2} destination group port-group {3}".format(
-                    ip_version, fw_table, rule, dest_port
-                )
-            )
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form["firewall_name"] != "":
+            session["firewall_name"] = request.form["firewall_name"]
+            return redirect(url_for("index"))
+    return render_template("login.html")
 
-    # Source
-    if source_address != "":
-        if source_address_type == "address":
-            config.append(
-                "set firewall {0} name {1} rule {2} source address {3}".format(
-                    ip_version, fw_table, rule, source_address
-                )
-            )
-        elif source_address_type == "address_group":
-            config.append(
-                "set firewall {0} name {1} rule {2} source group address-group {3}".format(
-                    ip_version, fw_table, rule, source_address
-                )
-            )
-        elif source_address_type == "network_group":
-            config.append(
-                "set firewall {0} name {1} rule {2} source group network-group {3}".format(
-                    ip_version, fw_table, rule, source_address
-                )
-            )
-    if source_port != "":
-        if source_port_type == "port":
-            config.append(
-                "set firewall {0} name {1} rule {2} source port {3}".format(
-                    ip_version, fw_table, rule, source_port
-                )
-            )
-        elif source_port_type == "port_group":
-            config.append(
-                "set firewall {0} name {1} rule {2} source group port-group {3}".format(
-                    ip_version, fw_table, rule, source_address
-                )
-            )
 
-    # Protocol
-    if protocol != "":
-        if ip_version == "ipv6" and protocol == "icmp":
-            protocol = "ipv6-icmp"
-        config.append(
-            "set firewall {0} name {1} rule {2} protocol {3}".format(
-                ip_version, fw_table, rule, protocol
-            )
-        )
-
-    # Logging
-    if logging:
-        config.append(
-            "set firewall {0} name {1} rule {2} log".format(ip_version, fw_table, rule)
-        )
-
-    # States
-    if state_est:
-        config.append(
-            "set firewall {0} name {1} rule {2} state 'established'".format(
-                ip_version, fw_table, rule
-            )
-        )
-    if state_inv:
-        config.append(
-            "set firewall {0} name {1} rule {2} state 'invalid'".format(
-                ip_version, fw_table, rule
-            )
-        )
-    if state_new:
-        config.append(
-            "set firewall {0} name {1} rule {2} state 'new'".format(
-                ip_version, fw_table, rule
-            )
-        )
-    if state_rel:
-        config.append(
-            "set firewall {0} name {1} rule {2} state 'related'".format(
-                ip_version, fw_table, rule
-            )
-        )
-
-    # Convert list of lines to single string
-    message = ""
-    for line in config:
-        message = message + line.replace("\n", "<br>") + "<br>"
-
-    # Create Jinja2 environment and get template
-    environment = Environment(loader=FileSystemLoader("templates/"))
-    template = environment.get_template("firewall_results.html")
-
-    # Render template with message
-    results = template.render(message=message)
-
-    # Return html page from rendered template
-    return results
+@app.route("/logout")
+def logout():
+    # remove the firewall_name from the session if it's there
+    session.pop("firewall_name", None)
+    session.clear()
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
-    env = os.environ.get("DEV") or False
+    # Look for FLASK_ENV environment variable.
+    env = os.environ.get("FLASK_ENV") or False
 
-    # If environment is DEV, then run app in debug mode on port 8080.
+    # If environment is set, run debug, else assume PROD
     if env:
         app.run(debug=True, host="0.0.0.0", port="8080")
     # Else, run app in production mode on port 8080.
---
+++ package/functions.py
@@ -0,0 +1,321 @@
+"""
+    Support functions.
+"""
+import json
+
+
+def delete_rule_from_data(session, request):
+    # Get user's data
+    user_data = read_user_data_file(session["firewall_name"])
+
+    # Set local vars from posted form data
+    ip_version = request.form["ip_version"]
+    fw_table = request.form["fw_table"]
+    rule = request.form["rule"]
+
+    # Delete rule from data
+    try:
+        del user_data[ip_version][fw_table][rule]
+    except:
+        pass
+
+    # Clean-up data
+    try:
+        if len(user_data[ip_version][fw_table]) == 0:
+            del user_data[ip_version][fw_table]
+        if len(user_data[ip_version]) == 0:
+            del user_data[ip_version]
+    except:
+        pass
+
+    # Write user's data to file
+    write_user_data_file(session["firewall_name"], user_data)
+
+    return
+
+
+def add_rule_to_data(session, request):
+    # Get user's data
+    user_data = read_user_data_file(session["firewall_name"])
+
+    # Set local vars from posted form data
+    ip_version = request.form["ip_version"]
+    fw_table = request.form["fw_table"]
+
+    if ip_version not in user_data:
+        user_data[ip_version] = {}
+        if fw_table not in user_data[ip_version]:
+            user_data[ip_version][fw_table] = {}
+
+    # Assemble rule dict
+    rule = request.form["rule"]
+    rule_dict = {}
+    rule_dict["description"] = request.form["description"]
+    if "rule_disable" in request.form:
+        rule_dict["rule_disable"] = True
+    if "logging" in request.form:
+        rule_dict["logging"] = True
+    rule_dict["action"] = request.form["action"]
+    rule_dict["dest_address"] = request.form["dest_address"]
+    rule_dict["dest_address_type"] = request.form["dest_address_type"]
+    rule_dict["dest_port"] = request.form["dest_port"]
+    rule_dict["dest_port_type"] = request.form["dest_port_type"]
+    rule_dict["source_address"] = request.form["source_address"]
+    rule_dict["source_address_type"] = request.form["source_address_type"]
+    rule_dict["source_port"] = request.form["source_port"]
+    rule_dict["source_port_type"] = request.form["source_port_type"]
+    rule_dict["protocol"] = (
+        request.form["protocol"] if "protocol" in request.form else ""
+    )
+    if "state_est" in request.form:
+        rule_dict["state_est"] = True
+    if "state_inv" in request.form:
+        rule_dict["state_inv"] = True
+    if "state_new" in request.form:
+        rule_dict["state_new"] = True
+    if "state_rel" in request.form:
+        rule_dict["state_rel"] = True
+
+    # Add rule to user_data
+    user_data[ip_version][fw_table][rule] = rule_dict
+
+    print(json.dumps(user_data, indent=4))
+
+    # Write user_data to file
+    write_user_data_file(session["firewall_name"], user_data)
+
+    return
+
+
+def read_user_data_file(filename):
+    try:
+        with open(f"data/{filename}.json", "r") as f:
+            data = f.read()
+            user_data = json.loads(data)
+            return user_data
+    except:
+        return {}
+
+
+def write_user_data_file(filename, data):
+    with open(f"data/{filename}.json", "w") as f:
+        f.write(json.dumps(data, indent=4))
+    return
+
+
+def generate_config(session):
+    # Get user data
+    user_data = read_user_data_file(session["firewall_name"])
+
+    # Create firewall configuration
+    config = []
+
+    # Work through each IP Version, Table and Rule adding to config
+    for ip_version in user_data:
+        config.append(f"#\n# {ip_version}\n#\n")
+
+        for fw_table in user_data[ip_version]:
+            config.append(f"#\n# Table: {fw_table}\n#\n")
+
+            for rule in user_data[ip_version][fw_table]:
+                for key in user_data[ip_version][fw_table][rule]:
+                    description = user_data[ip_version][fw_table][rule]["description"]
+                    rule_disable = (
+                        True
+                        if "rule_disable" in user_data[ip_version][fw_table][rule]
+                        else False
+                    )
+                    logging = (
+                        True
+                        if "logging" in user_data[ip_version][fw_table][rule]
+                        else False
+                    )
+                    action = user_data[ip_version][fw_table][rule]["action"]
+                    dest_address = user_data[ip_version][fw_table][rule]["dest_address"]
+                    dest_address_type = user_data[ip_version][fw_table][rule][
+                        "dest_address_type"
+                    ]
+                    dest_port = user_data[ip_version][fw_table][rule]["dest_port"]
+                    dest_port_type = user_data[ip_version][fw_table][rule][
+                        "dest_port_type"
+                    ]
+                    source_address = user_data[ip_version][fw_table][rule][
+                        "source_address"
+                    ]
+                    source_address_type = user_data[ip_version][fw_table][rule][
+                        "source_address_type"
+                    ]
+                    source_port = user_data[ip_version][fw_table][rule]["source_port"]
+                    source_port_type = user_data[ip_version][fw_table][rule][
+                        "source_port_type"
+                    ]
+                    protocol = (
+                        user_data[ip_version][fw_table][rule]["protocol"]
+                        if "protocol" in rule
+                        else ""
+                    )
+                    state_est = (
+                        True
+                        if "state_est" in user_data[ip_version][fw_table][rule]
+                        else False
+                    )
+                    state_inv = (
+                        True
+                        if "state_inv" in user_data[ip_version][fw_table][rule]
+                        else False
+                    )
+                    state_new = (
+                        True
+                        if "state_new" in user_data[ip_version][fw_table][rule]
+                        else False
+                    )
+                    state_rel = (
+                        True
+                        if "state_rel" in user_data[ip_version][fw_table][rule]
+                        else False
+                    )
+
+                config.append("# Rule {0}".format(rule))
+
+                # Disable
+                if rule_disable:
+                    config.append(
+                        "set firewall {0} name {1} rule {2} disable".format(
+                            ip_version, fw_table, rule
+                        )
+                    )
+
+                # Description
+                config.append(
+                    "set firewall {0} name {1} rule {2} description '{3}'".format(
+                        ip_version, fw_table, rule, description
+                    )
+                )
+
+                # Action
+                config.append(
+                    "set firewall {0} name {1} rule {2} action {3}".format(
+                        ip_version, fw_table, rule, action
+                    )
+                )
+
+                # Destination
+                if dest_address != "":
+                    if dest_address_type == "address":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} destination address {3}".format(
+                                ip_version, fw_table, rule, dest_address
+                            )
+                        )
+                    elif dest_address_type == "address_group":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} destination group address-group {3}".format(
+                                ip_version, fw_table, rule, dest_address
+                            )
+                        )
+                    elif dest_address_type == "network_group":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} destination group network-group {3}".format(
+                                ip_version, fw_table, rule, dest_address
+                            )
+                        )
+                if dest_port != "":
+                    if dest_port_type == "port":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} destination port {3}".format(
+                                ip_version, fw_table, rule, dest_port
+                            )
+                        )
+                    elif dest_port_type == "port_group":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} destination group port-group {3}".format(
+                                ip_version, fw_table, rule, dest_port
+                            )
+                        )
+
+                # Source
+                if source_address != "":
+                    if source_address_type == "address":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} source address {3}".format(
+                                ip_version, fw_table, rule, source_address
+                            )
+                        )
+                    elif source_address_type == "address_group":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} source group address-group {3}".format(
+                                ip_version, fw_table, rule, source_address
+                            )
+                        )
+                    elif source_address_type == "network_group":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} source group network-group {3}".format(
+                                ip_version, fw_table, rule, source_address
+                            )
+                        )
+                if source_port != "":
+                    if source_port_type == "port":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} source port {3}".format(
+                                ip_version, fw_table, rule, source_port
+                            )
+                        )
+                    elif source_port_type == "port_group":
+                        config.append(
+                            "set firewall {0} name {1} rule {2} source group port-group {3}".format(
+                                ip_version, fw_table, rule, source_address
+                            )
+                        )
+
+                # Protocol
+                if protocol != "":
+                    if ip_version == "ipv6" and protocol == "icmp":
+                        protocol = "ipv6-icmp"
+                    config.append(
+                        "set firewall {0} name {1} rule {2} protocol {3}".format(
+                            ip_version, fw_table, rule, protocol
+                        )
+                    )
+
+                # Logging
+                if logging:
+                    config.append(
+                        "set firewall {0} name {1} rule {2} log".format(
+                            ip_version, fw_table, rule
+                        )
+                    )
+
+                # States
+                if state_est:
+                    config.append(
+                        "set firewall {0} name {1} rule {2} state 'established'".format(
+                            ip_version, fw_table, rule
+                        )
+                    )
+                if state_inv:
+                    config.append(
+                        "set firewall {0} name {1} rule {2} state 'invalid'".format(
+                            ip_version, fw_table, rule
+                        )
+                    )
+                if state_new:
+                    config.append(
+                        "set firewall {0} name {1} rule {2} state 'new'".format(
+                            ip_version, fw_table, rule
+                        )
+                    )
+                if state_rel:
+                    config.append(
+                        "set firewall {0} name {1} rule {2} state 'related'".format(
+                            ip_version, fw_table, rule
+                        )
+                    )
+                config.append("")
+
+    # Convert list of lines to single string
+    message = ""
+    for line in config:
+        message = message + line.replace("\n", "<br>") + "<br>"
+
+    # Return message of config commands
+    return message
