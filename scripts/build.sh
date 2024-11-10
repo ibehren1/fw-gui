a4926604c2a43f23c46534c8f5d0f88c24f5334b
Isaac Behrens <isaac@behrenshome.com>
2024-11-10 14:09:45 -0600

    Update build script with niceities and to populate .version from pyproject.toml.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # Script to run Docker build.
 
+# Internal registry for publishing dev builds.
+INTERNAL_REG="registry.internal.behrenshome.com"
+
+
+#
+# Set some colors.
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+
+#
 # Determine which tag to apply to image.
 if [ "${1}" == "Prod" ]; then
    BUILD_TYPE="Prod"
@@ -11,17 +25,31 @@ else
 fi
 
 # Read the version number.
-VERSION=$(<.version)
-echo -e "\nBuilding version: ${VERSION}:${BUILD_TYPE}\n"
+VERSION=`python3 -c 'import toml; c = toml.load("pyproject.toml"); print((c["project"]["version"]))'`
+echo -e "\n${CYAN}Building version: ${VERSION}:${BUILD_TYPE}${NC}\n"
+echo -e "v${VERSION}" > .version
 
+
+#
 # Run the bandit security scan on the codebase.
-echo -e "\nRunning Bandit security scan on codebase.\n"
-bandit -r .
+echo -e "\n${CYAN}#\n# Running Bandit security scan on codebase.${NC}\n"
+bandit -c pyproject.toml -r .
 if [ $? -ne 0 ]; then
-    echo -e "\n***\n*\n*   Error - Bandit security scan failed. Exiting.\n*\n***"
+    echo -e "{${RED}\n***\n*\n*   Error - Bandit security scan failed. Exiting.\n*\n***${NC}"
     exit 1
 fi
-echo -e "\nBandit security scan completed.\n"
+echo -e "\n${GREEN}Bandit security scan completed.${NC}\n"
+
+
+#
+# Run pytest to validate the codebase.
+echo -e "\n${CYAN}#\n# Running Pytest on codebase.${NC}\n"
+pytest
+
+
+#
+# Run Docker Build and Publish
+echo -e "\n${CYAN}#\n# Running Dockerbuild.${NC}\n"
 
 # Set the Docker Hub username and password
 docker login -u ${DOCKER_USER} -p ${DOCKER_PAT}
@@ -50,8 +78,8 @@ if [ "${BUILD_TYPE}" == "PubDev" ]; then
         --no-cache \
         --push \
         --tag ${DOCKER_USER}/fw-gui:dev-${VERSION} \
-        --tag registry.internal.behrenshome.com/fw-gui:dev-${VERSION} \
-        --tag registry.internal.behrenshome.com/fw-gui:dev-latest .
+        --tag ${INTERNAL_REG}/fw-gui:dev-${VERSION} \
+        --tag ${INTERNAL_REG}/fw-gui:dev-latest .
 fi
 
 if [ "${BUILD_TYPE}" == "Dev" ]; then
@@ -59,8 +87,10 @@ if [ "${BUILD_TYPE}" == "Dev" ]; then
         --platform=linux/arm64,linux/amd64 \
         --no-cache \
         --push \
-        --tag registry.internal.behrenshome.com/fw-gui:dev-${VERSION} \
-        --tag registry.internal.behrenshome.com/fw-gui:dev-latest .
+        --tag ${INTERNAL_REG}/fw-gui:dev-${VERSION} \
+        --tag ${INTERNAL_REG}/fw-gui:dev-latest .
 fi
 
-echo -e "\n${BUILD_TYPE} Build of ${VERSION} completed."
+echo -e "\n${GREEN}${BUILD_TYPE} Build of ${VERSION} completed.${NC}"
+
+exit 0
